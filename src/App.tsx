78aeda55ab149101c79276d3094b7c004b83c5f3
Jack Hutson <jack.n.hutson@gmail.com>
2021-07-11 15:17:24 -0400

    working on react grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Grid from './grid/Grid';
 import LifeService from './services/life.service';
 
-function App() {
-  let lifeService = new LifeService();
+interface AppState {
+  life: LifeService;
+  grid: boolean[][];
+  active: boolean;
+}
 
-  console.log(lifeService);
+interface Props {
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        Jack's Solution for Conway's Game of Life
-      </header>
-      <Grid gridState={lifeService.grid}></Grid>
-    </div>
-  );
+}
+
+class App extends React.Component<Props, AppState> {
+
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      life: new LifeService(),
+      grid: [],
+      active: false,
+    }
+  }
+  
+  playClick = () => {
+    this.setState({
+      grid: this.state.life.seedGrid(),
+      active: true,
+    })
+  }
+
+  pauseClick = () => {
+    this.setState({
+      active: false,
+    })
+  }
+
+  stopClick = () => {
+    this.setState({
+      grid: [],
+      active: false,
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>Jack's Solution for Conway's Game of Life</div>
+          <div className="tools">
+            <div onClick={this.playClick}>Play</div>
+            <div>Pause</div>
+            <div onClick={this.stopClick}>Stop</div>
+          </div>
+        </header>
+        <Grid gridState={this.state.grid}></Grid>
+      </div>
+    );
+  }
 }
 
 export default App;
